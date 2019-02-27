Improved backpropagation for 2 hidden layers, not 100% sure if its correct

activation_function.py
@@ -1,10 +1,15 @@
 import numpy as np
 
 
-
+'''
+TODO
+'''
 def relu():
     pass
 
+'''
+Squashes values in the range between 0 and 1
+'''
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 